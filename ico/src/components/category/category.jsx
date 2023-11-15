bb1eb94a57f74d09d892154ad7f4613ff0b39924
nem0z <maxime.bernard.dev@gmail.com>
2023-11-15 16:38:42 +0100

    feat (ico/src/components/category): implement add line system

--- ico/src/components/category/category.jsx
+++ ico/src/components/category/category.jsx
@@ -1,19 +1,34 @@
 import './style.scss';
+
+import { useEffect, useRef, useState } from 'react';
+
 import Item from '../item/item';
 
 function Category({category, categoryName}) {
+
+	const [lines, setLines] = useState([{ total: 0 }]);
+	const btnAdd = useRef(null);
+
+	useEffect(() => {
+		const handler = () => setLines(prev => [...prev, { total: 0 }]);
+		btnAdd.current?.addEventListener('click', handler);
+		
+		return () => btnAdd.current?.removeEventListener('click', handler);
+	}, []);
 
 	return (
 		<section className='category'>
 			<div className='titleContainer'>
 				<h2>{categoryName}</h2>
-				<div className='btnAdd'>
+				<div className='btnAdd' ref={btnAdd}>
 					<span>+</span>
 				</div>
 			</div>
 
 			<div>
-				<Item category={category}/>
+				{
+					lines.map((_, i) => <Item key={i} category={category}/>)
+				}
 			</div>
 
 		</section>
