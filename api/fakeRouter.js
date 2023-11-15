114ea4ab5701321d7f03052c21cd4bd2e94435d5
nem0z <maxime.bernard.dev@gmail.com>
2023-11-15 09:36:08 +0100

    feat (api): implement method to retrive Fe and FeUnit filtered

--- api/fakeRouter.js
+++ api/fakeRouter.js
@@ -9,4 +9,18 @@ const getCaterogies = function () {
     });
 }
 
-export { getCaterogies };
+const getFe = function (category) {
+    return new Promise((resolve, reject) => {
+        resolve(Fe.filter(x => x.category == category));
+        reject(new Error("An error occured"));
+    });
+}
+
+const getFeUnits = function (fe) {
+    return new Promise((resolve, reject) => {
+        resolve(FeUnit.filter(x => x.fe == fe));
+        reject(new Error("An error occured"));
+    });
+}
+
+export { getCaterogies, getFe, getFeUnits };
--- api/index.js
+++ api/index.js
@@ -1,3 +1,9 @@
-import { getCaterogies } from "./fakeRouter.js";
+import { getCaterogies, getFe, getFeUnits } from "./fakeRouter.js";
 
 getCaterogies().then(console.log);
+
+getFe(5).then(console.log);
+getFe(1).then(console.log);
+
+getFeUnits(1).then(console.log);
+getFeUnits(3).then(console.log);
