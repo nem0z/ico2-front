4b650e5ee69a5df544f5afa4e322052a6027dc21
nem0z <maxime.bernard.dev@gmail.com>
2023-11-20 15:13:52 +0100

    feat (ico/src/components/category): calculate / display and send the total to the App component

--- ico/src/components/category/category.jsx
+++ ico/src/components/category/category.jsx
@@ -1,23 +1,33 @@
 import './style.scss';
 
-import { useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import Item from '../item/item';
+import { sumOf } from '../../utils.js';
 
-function Category({category, categoryName}) {
+
+function Category({category, categoryName, onCalc}) {
 
 	const [lines, setLines] = useState([]);
+	const [total, setTotal] = useState(0);
 
 	const addLine = () => setLines(prev => [...prev, { total: 0 }]);
 
-	const removeLine = index => {
-		setLines(prev => prev.filter((_, i) => i !== index));
-	};
+	const removeLine = index => setLines(prev => prev.filter((_, i) => i !== index));
+
+	const handleLine = (index, line) => setLines(prev => prev.map((l, i) => i==index ? line : l));
+
+	useEffect(() => {
+		setTotal(sumOf(lines, "total"));
+
+		onCalc(lines);
+	}, [lines]);
 
 	return (
 		<section className='category'>
 			<div className='titleContainer'>
-				<h2>{categoryName}</h2>
+				<h2>{ categoryName }</h2>
+				<span>{ Math.round(total) }</span>
 				<div className='btnAdd' onClick={addLine}>
 					<span>+</span>
 				</div>
@@ -29,7 +39,8 @@ function Category({category, categoryName}) {
 						<Item 
 							key={i} 
 							category={category}
-							onRemove={() => removeLine(i)} 
+							onRemove={() => removeLine(i)}
+							onCalc={line => handleLine(i, line)}
 						/>
 					)
 				}
