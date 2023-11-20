0c8ae874f899c1e1639884a95c931195fd94c6df
nem0z <maxime.bernard.dev@gmail.com>
2023-11-20 10:45:49 +0100

    fix (ico/src/component/category): change class to className

--- ico/src/components/category/category.jsx
+++ ico/src/components/category/category.jsx
@@ -23,7 +23,7 @@ function Category({category, categoryName}) {
 				</div>
 			</div>
 
-			<div class="categoryItems">
+			<div className="categoryItems">
 				{
 					lines.map((_, i) => 
 						<Item 
